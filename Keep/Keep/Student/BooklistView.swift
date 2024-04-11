7f0a47a749610e9924d1d9e9ddd82f23d735a404
Kim ju hwan <minecraft_bos@naver.com>
2024-04-11 18:16:39 +0900

    ✨ BooklistView

--- Keep/Keep/Student/BooklistView.swift
+++ Keep/Keep/Student/BooklistView.swift
@@ -9,7 +9,111 @@ import SwiftUI
 
 struct BooklistView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("나의 대출 목록")
+            .font(.system(size: 28, weight: .bold))
+            .padding(.trailing,170)
+        Rectangle()
+            .frame(height:1)
+            .foregroundColor(.gray)
+        ScrollView {
+            VStack {
+                HStack(spacing:30) {
+                    Image("book1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:120)
+                    VStack(alignment: .leading, spacing: 20) {
+                        VStack(alignment: .leading, spacing: 3) {
+                            Text("인간실격")
+                                .font(.system(size: 20,weight:.semibold))
+                            Text("디자이 오사무")
+                                .font(.system(size: 12))
+                        }
+                        VStack(alignment: .leading, spacing: 3) {
+                            Text("대출일")
+                                .font(.system(size: 12))
+                            Text("2024.03.28")
+                                .font(.system(size: 12))
+                                .foregroundColor(.blue)
+                        }
+                        VStack(alignment: .leading, spacing: 3) {
+                            Text("반납 예정일")
+                                .font(.system(size: 12))
+                            Text("2024.04.11")
+                                .font(.system(size: 12))
+                                .foregroundColor(.red)
+                        }
+                    }
+                }
+                .padding()
+                Rectangle()
+                    .frame(width:500,height:1)
+                    .foregroundColor(.gray)
+                HStack(spacing:30) {
+                    Image("book1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:120)
+                    VStack(alignment: .leading, spacing: 20) {
+                        VStack(alignment: .leading, spacing: 3) {
+                            Text("인간실격")
+                                .font(.system(size: 20,weight:.semibold))
+                            Text("디자이 오사무")
+                                .font(.system(size: 12))
+                        }
+                        VStack(alignment: .leading, spacing: 3) {
+                            Text("대출일")
+                                .font(.system(size: 12))
+                            Text("2024.03.28")
+                                .font(.system(size: 12))
+                                .foregroundColor(.blue)
+                        }
+                        VStack(alignment: .leading, spacing: 3) {
+                            Text("반납 예정일")
+                                .font(.system(size: 12))
+                            Text("2024.04.11")
+                                .font(.system(size: 12))
+                                .foregroundColor(.red)
+                        }
+                    }
+                }
+                .padding()
+                Rectangle()
+                    .frame(width:500,height:1)
+                    .foregroundColor(.gray)
+                HStack(spacing:30) {
+                    Image("book1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:120)
+                    VStack(alignment: .leading, spacing: 20) {
+                        VStack(alignment: .leading, spacing: 3) {
+                            Text("인간실격")
+                                .font(.system(size: 20,weight:.semibold))
+                            Text("디자이 오사무")
+                                .font(.system(size: 12))
+                        }
+                        VStack(alignment: .leading, spacing: 3) {
+                            Text("대출일")
+                                .font(.system(size: 12))
+                            Text("2024.03.28")
+                                .font(.system(size: 12))
+                                .foregroundColor(.blue)
+                        }
+                        VStack(alignment: .leading, spacing: 3) {
+                            Text("반납 예정일")
+                                .font(.system(size: 12))
+                            Text("2024.04.11")
+                                .font(.system(size: 12))
+                                .foregroundColor(.red)
+                        }
+                    }
+                }
+                .padding()
+            }
+            .padding(.trailing,100)
+        }
+        .frame(height:600)
     }
 }
 
