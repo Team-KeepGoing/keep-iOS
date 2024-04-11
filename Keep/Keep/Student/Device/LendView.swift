e8a758b7c5a6519df6d0ce0ae1ce4b4350ee6d75
Kim ju hwan <minecraft_bos@naver.com>
2024-04-12 08:35:43 +0900

    ✨ LendView

--- Keep/Keep/Student/Device/LendView.swift
+++ Keep/Keep/Student/Device/LendView.swift
@@ -9,8 +9,74 @@ import SwiftUI
 
 struct LendView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        Text("기기 대여")
+            .font(.system(size: 28, weight: .bold))
+            .padding(.trailing,200)
+        ScrollView {
+            VStack(alignment: .leading, spacing:10) {
+                HStack {
+                    Image("macbook")
+                        .resizable()
+                        .frame(width:64,height: 64)
+                    VStack(alignment:.leading, spacing:3) {
+                        Text("노트북")
+                            .font(.system(size: 20, weight: .bold))
+                        Rectangle()
+                            .frame(width:45,height:15)
+                            .cornerRadius(7)
+                            .foregroundColor(.red)
+                            .overlay(
+                                Text("사용 중")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 10, weight: .thin))
+                            )
+                    }
+                }
+                .padding(.trailing,100)
+                HStack {
+                    Image("macbook")
+                        .resizable()
+                        .frame(width:64,height: 64)
+                    VStack(alignment:.leading, spacing:3) {
+                        Text("노트북")
+                            .font(.system(size: 20, weight: .bold))
+                        Rectangle()
+                            .frame(width:45,height:15)
+                            .cornerRadius(7)
+                            .foregroundColor(.red)
+                            .overlay(
+                                Text("사용 중")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 10, weight: .thin))
+                            )
+                    }
+                }
+                .padding(.trailing,100)
+
+                HStack {
+                    Image("macbook")
+                        .resizable()
+                        .frame(width:64,height: 64)
+                    VStack(alignment:.leading, spacing:3) {
+                        Text("노트북")
+                            .font(.system(size: 20, weight: .bold))
+                        Rectangle()
+                            .frame(width:45,height:15)
+                            .cornerRadius(7)
+                            .foregroundColor(.red)
+                            .overlay(
+                                Text("사용 중")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 10, weight: .thin))
+                            )
+                    }
+                }
+                .padding(.trailing,100)
+
+            }
+            .padding(.trailing,100)
+        }
+        .frame(height:600)    }
 }
 
 #Preview {
