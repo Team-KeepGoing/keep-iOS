66aaf0dbfff1bdac96ada4dfeb73d6eed5aad6eb
Kim ju hwan <minecraft_bos@naver.com>
2024-04-13 12:44:36 +0900

    ✨ LendlistView

--- Keep/Keep/Student/Device/LendlistView.swift
+++ Keep/Keep/Student/Device/LendlistView.swift
@@ -9,7 +9,53 @@ import SwiftUI
 
 struct LendlistView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("나의 대여 목록")
+                .font(.system(size: 28, weight: .bold))
+                .padding(.trailing,170)
+            Rectangle()
+                .frame(height:1)
+                .foregroundColor(.gray)
+            ScrollView {
+                VStack(spacing:15) {
+                    Rectangle()
+                        .frame(width:340,height:60)
+                        .foregroundColor(.buttoncolor)
+                        .cornerRadius(15)
+                        .overlay(
+                            HStack(spacing: 120) {
+                                Text("아이패드")
+                                    .font(.system(size: 22, weight: .semibold))
+                                Text("2024/04/13")
+                            }
+                        )
+                    Rectangle()
+                        .frame(width:340,height:60)
+                        .foregroundColor(.buttoncolor)
+                        .cornerRadius(15)
+                        .overlay(
+                            HStack(spacing: 120) {
+                                Text("아이패드")
+                                    .font(.system(size: 22, weight: .semibold))
+                                Text("2024/04/13")
+                            }
+                        )
+                    Rectangle()
+                        .frame(width:340,height:60)
+                        .foregroundColor(.buttoncolor)
+                        .cornerRadius(15)
+                        .overlay(
+                            HStack(spacing: 120) {
+                                Text("아이패드")
+                                    .font(.system(size: 22, weight: .semibold))
+                                Text("2024/04/13")
+                            }
+                        )
+                }
+            }
+            .padding()
+            .frame(height:600)
+        }
     }
 }
 
