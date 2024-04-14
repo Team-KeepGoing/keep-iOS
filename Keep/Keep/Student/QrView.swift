6c872d99e89d8902f565c768a95815d57aec2afd
Kim ju hwan <minecraft_bos@naver.com>
2024-04-14 23:25:41 +0900

    ✨ QrView

--- Keep/Keep/Student/QrView.swift
+++ Keep/Keep/Student/QrView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct QrView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text("QR 대여")
+                .font(.system(size: 28, weight: .semibold))
+            Spacer()
+                .frame(height:30)
+            VStack(alignment: .leading) {
+                Text("도서관에 배치 되어 있는")
+                Text("QR코드 인식기에")
+                Text("QR을 인식해주세요!")
+            }
+            .font(.system(size: 22, weight: .thin))
+        }
+        .padding(.trailing, 70)
     }
 }
 
