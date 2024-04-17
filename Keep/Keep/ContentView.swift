8c53c597edc6dd545b8d122dbe3b7e2563b4782d
bibiga07 <130065479+bibiga07@users.noreply.github.com>
2024-04-17 22:31:13 +0900

    ✨ ContentView NavigationView

--- Keep/Keep/ContentView.swift
+++ Keep/Keep/ContentView.swift
@@ -9,43 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image("Logo")
-                .resizable()
-                .scaledToFit()
-                .frame(width:130)
-            Text("대출/반납을 더욱 간편하게")
-                .font(.system(size:16,weight:.thin))
-        }
-        .padding(30)
-        VStack {
-            Button(action: {
+        NavigationView {
+            VStack {
+                Image("Logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 130)
+                Text("대출/반납을 더욱 간편하게")
+                    .font(.system(size: 16, weight: .thin))
                 
-            }, label: {
-                Rectangle()
-                    .foregroundColor(.buttoncolor)
-                    .frame(width: 310, height: 60)
-                    .cornerRadius(20)
-                    .overlay(
+                Spacer()
+                    .frame(height:40)
+                
+                NavigationLink(destination: LoginView()) {
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.buttoncolor)
+                            .frame(width: 310, height: 60)
+                            .cornerRadius(20)
                         Text("로그인")
                             .foregroundColor(.black)
                             .font(.system(size: 30, weight: .bold, design: .default))
-                    )
-            })
-            Button(action: {
-
-            }, label: {
-                Rectangle()
-                    .foregroundColor(.buttoncolor)
-                    .frame(width: 310, height: 60)
-                    .cornerRadius(20)
-                    .overlay(
+                    }
+                }
+                .padding(.bottom)
+                
+                NavigationLink(destination: SignupView()) {
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.buttoncolor)
+                            .frame(width: 310, height: 60)
+                            .cornerRadius(20)
                         Text("회원가입")
                             .foregroundColor(.black)
                             .font(.system(size: 30, weight: .bold, design: .default))
-                    )
-            })
-            .padding()
+                    }
+                }
+            }
         }
     }
 }
