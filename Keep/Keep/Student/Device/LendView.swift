30fe570195979336f27ba56c5af4590e3334fe9a
bibiga07 <130065479+bibiga07@users.noreply.github.com>
2024-05-02 23:47:12 +0900

    🎨 Refactoring

--- Keep/Keep/Student/Device/LendView.swift
+++ Keep/Keep/Student/Device/LendView.swift
@@ -7,8 +7,38 @@
 
 import SwiftUI
 
+import SwiftUI
+
+struct ELend: View {
+    var image: String
+    var title: String
+    var lendStatus: String
+
+    var body: some View {
+        HStack {
+            Image(image)
+                .resizable()
+                .frame(width: 64, height: 64)
+            VStack(alignment:.leading, spacing:3) {
+                Text(title)
+                    .font(.system(size: 20, weight: .bold))
+                Rectangle()
+                    .frame(width:45,height:15)
+                    .cornerRadius(7)
+                    .foregroundColor(.red)
+                    .overlay(
+                        Text(lendStatus)
+                            .foregroundColor(.white)
+                            .font(.system(size: 10, weight: .thin))
+                    )
+            }
+        }
+        .padding(.trailing,100)
+    }
+}
+
 struct LendView: View {
-    @State private var LendUsing: String = "사용 중"
+    @State private var lendUsing: String = "사용 중"
     var body: some View {
         VStack {
             Text("기기 대여")
@@ -16,31 +46,16 @@ struct LendView: View {
                 .padding(.trailing,200)
             ScrollView {
                 VStack(alignment: .leading, spacing:10) {
-                    HStack {
-                        Image("macbook")
-                            .resizable()
-                            .frame(width:64,height: 64)
-                        VStack(alignment:.leading, spacing:3) {
-                            Text("노트북")
-                                .font(.system(size: 20, weight: .bold))
-                            Rectangle()
-                                .frame(width:45,height:15)
-                                .cornerRadius(7)
-                                .foregroundColor(.red)
-                                .overlay(
-                                    Text(LendUsing)
-                                        .foregroundColor(.white)
-                                        .font(.system(size: 10, weight: .thin))
-                                )
-                        }
-                    }
-                    .padding(.trailing,100)
+                    ELend(image: "macbook", title: "노트북", lendStatus: lendUsing)
+                    ELend(image: "macbook", title: "노트북", lendStatus: lendUsing)
                 }
                 .padding(.trailing,100)
             }
-            .frame(height:600)}
+            .frame(height:600)
+        }
     }
 }
+
 
 #Preview {
     LendView()
