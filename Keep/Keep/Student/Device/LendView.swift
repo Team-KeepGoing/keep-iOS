ffc73117e265de8d6ec79e63f5f6e52f40ac86a8
bibiga07 <130065479+bibiga07@users.noreply.github.com>
2024-05-06 23:46:01 +0900

    LendView -> LendDetailView

--- Keep/Keep/Student/Device/LendView.swift
+++ Keep/Keep/Student/Device/LendView.swift
@@ -13,45 +13,52 @@ struct ELend: View {
     var image: String
     var title: String
     var lendStatus: String
-
+    
     var body: some View {
-        HStack {
-            Image(image)
-                .resizable()
-                .frame(width: 64, height: 64)
-            VStack(alignment:.leading, spacing:3) {
-                Text(title)
-                    .font(.system(size: 20, weight: .bold))
-                Rectangle()
-                    .frame(width:45,height:15)
-                    .cornerRadius(7)
-                    .foregroundColor(.red)
-                    .overlay(
-                        Text(lendStatus)
-                            .foregroundColor(.white)
-                            .font(.system(size: 10, weight: .thin))
-                    )
+        NavigationLink(destination: LendDetailView()) {
+            HStack {
+                Image(image)
+                    .resizable()
+                    .frame(width: 64, height: 64)
+                VStack(alignment:.leading, spacing:3) {
+                    Text(title)
+                        .font(.system(size: 20, weight: .bold))
+                        .foregroundColor(.black)
+                    Rectangle()
+                        .frame(width:45,height:15)
+                        .cornerRadius(7)
+                        .foregroundColor(.red)
+                        .overlay(
+                            Text(lendStatus)
+                                .foregroundColor(.white)
+                                .font(.system(size: 10, weight: .thin))
+                        )
+                }
             }
+            .padding(.trailing,100)
         }
-        .padding(.trailing,100)
     }
 }
 
 struct LendView: View {
     @State private var lendUsing: String = "사용 중"
     var body: some View {
-        VStack {
-            Text("기기 대여")
-                .font(.system(size: 28, weight: .bold))
-                .padding(.trailing,200)
-            ScrollView {
-                VStack(alignment: .leading, spacing:10) {
-                    ELend(image: "macbook", title: "노트북", lendStatus: lendUsing)
-                    ELend(image: "macbook", title: "노트북", lendStatus: lendUsing)
+        NavigationView {
+            VStack {
+                Text("기기 대여")
+                    .font(.system(size: 28, weight: .bold))
+                    .padding(.trailing,200)
+                ScrollView {
+                    VStack(alignment: .leading, spacing:10) {
+                        ELend(image: "macbook", title: "노트북", lendStatus: lendUsing)
+                        ELend(image: "macbook", title: "노트북", lendStatus: lendUsing)
+                    }
+                    .padding(.trailing,100)
                 }
-                .padding(.trailing,100)
+                .frame(height:600)
             }
-            .frame(height:600)
+            .navigationBarTitle("")
+            .navigationBarHidden(true)
         }
     }
 }
