0dc44cf0b9f5d59b059c0cb153d33705ed0d01bf
bibiga07 <130065479+bibiga07@users.noreply.github.com>
2024-05-07 09:24:33 +0900

    ✨ HomeView Navigation

--- Keep/Keep/Student/HomeView.swift
+++ Keep/Keep/Student/HomeView.swift
@@ -9,180 +9,177 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ZStack {
-            Rectangle()
-                .foregroundColor(.buttoncolor)
-                .frame(height:800)
-                .offset(y: -60)
-            
-            Image("Character")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 44)
-                .offset(x:-150,y:-350)
-            
-            ScrollView {
+        NavigationView {
+            ZStack {
                 Rectangle()
-                    .frame(width:350,height:200)
-                    .foregroundColor(.white)
-                    .cornerRadius(15)
-                    .padding()
-                    .overlay(
-                        VStack {
-                            HStack {
-                                Image(systemName: "qrcode")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width:23)
-                                    .padding(5)
-                                Text("도서 대출")
-                                    .font(.system(size: 23, weight: .bold))
-                                Spacer()
-                                    .frame(width:150)
-                                Button {
-                                    
-                                } label: {
-                                    Image(systemName: "arrow.right")
+                    .foregroundColor(.buttoncolor)
+                    .frame(height:800)
+                    .offset(y: -60)
+                
+                Image("Character")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 44)
+                    .offset(x:-150,y:-350)
+                
+                ScrollView {
+                    Rectangle()
+                        .frame(width:350,height:200)
+                        .foregroundColor(.white)
+                        .cornerRadius(15)
+                        .padding()
+                        .overlay(
+                            VStack {
+                                HStack {
+                                    Image(systemName: "qrcode")
                                         .resizable()
                                         .scaledToFit()
-                                        .foregroundColor(.black)
-                                        .frame(width:20)
-                                }
-                            }
-                            
-                            Button {
-                                
-                            } label: {
-                                Rectangle()
-                                    .foregroundColor(.buttoncolor)
-                                    .frame(width:125,height:35)
-                                    .cornerRadius(10)
-                                    .overlay(
-                                        Text("나의 대출 목록")
+                                        .frame(width:23)
+                                        .padding(5)
+                                    Text("도서 대출")
+                                        .font(.system(size: 23, weight: .bold))
+                                    Spacer()
+                                        .frame(width:150)
+                                    NavigationLink(destination: QrView()) {
+                                        Image(systemName: "arrow.right")
+                                            .resizable()
+                                            .scaledToFit()
                                             .foregroundColor(.black)
-                                            .font(.system(size: 17, weight: .semibold))
-                                    )
+                                            .frame(width:20)
+                                    }
+                                }
+                                
+                                NavigationLink(destination: BooklistView()) {
+                                    Rectangle()
+                                        .foregroundColor(.buttoncolor)
+                                        .frame(width:125,height:35)
+                                        .cornerRadius(10)
+                                        .overlay(
+                                            Text("나의 대출 목록")
+                                                .foregroundColor(.black)
+                                                .font(.system(size: 17, weight: .semibold))
+                                        )
+                                }
+                                .padding(.leading,180)
+                                
+                                VStack(alignment: .leading) {
+                                    Text("회원님이 대출중인 책")
+                                        .bold()
+                                    Text("미래의 사랑 이야기")
+                                        .font(.system(size: 15, weight: .thin))
+                                    Text("화를 극복해내는 방법")
+                                        .font(.system(size: 15, weight: .thin))
+                                }
+                                .offset(x:-80,y:10)
+                                
                             }
-                            .padding(.leading,180)
-                            
-                            VStack(alignment: .leading) {
-                                Text("회원님이 대출중인 책")
-                                    .bold()
-                                Text("미래의 사랑 이야기")
-                                    .font(.system(size: 15, weight: .thin))
-                                Text("화를 극복해내는 방법")
-                                    .font(.system(size: 15, weight: .thin))
-                            }
-                            .offset(x:-80,y:10)
-                            
-                        }
-                    )
-                Rectangle()
-                    .frame(width:350,height:420)
-                    .foregroundColor(.white)
-                    .cornerRadius(15)
-                    .overlay(
-                        VStack {
-                            HStack {
-                                Image(systemName: "printer")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width:23)
-                                    .padding(5)
-                                Text("기기 대여")
-                                    .font(.system(size: 23, weight: .bold))
-                                Spacer()
-                                    .frame(width:150)
-                                Button {
-                                    
-                                } label: {
-                                    Image(systemName: "arrow.right")
+                        )
+                    Rectangle()
+                        .frame(width:350,height:420)
+                        .foregroundColor(.white)
+                        .cornerRadius(15)
+                        .overlay(
+                            VStack {
+                                HStack {
+                                    Image(systemName: "printer")
                                         .resizable()
                                         .scaledToFit()
-                                        .foregroundColor(.black)
-                                        .frame(width:20)
+                                        .frame(width:23)
+                                        .padding(5)
+                                    Text("기기 대여")
+                                        .font(.system(size: 23, weight: .bold))
+                                    Spacer()
+                                        .frame(width:150)
+                                    
+                                    NavigationLink(destination: LendView()) {
+                                        Image(systemName: "arrow.right")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .foregroundColor(.black)
+                                            .frame(width:20)
+                                    }
                                 }
+                                VStack(spacing:10) {
+                                    HStack {
+                                        Image("macbook")
+                                            .resizable()
+                                            .frame(width:64,height: 64)
+                                        VStack(alignment:.leading, spacing:3) {
+                                            Text("노트북")
+                                                .font(.system(size: 20, weight: .bold))
+                                            Rectangle()
+                                                .frame(width:45,height:15)
+                                                .cornerRadius(7)
+                                                .foregroundColor(.red)
+                                                .overlay(
+                                                    Text("사용 중")
+                                                        .foregroundColor(.white)
+                                                        .font(.system(size: 10, weight: .thin))
+                                                )
+                                        }
+                                    }
+                                    HStack {
+                                        Image("macbook")
+                                            .resizable()
+                                            .frame(width:64,height: 64)
+                                        VStack(alignment:.leading, spacing:3) {
+                                            Text("노트북")
+                                                .font(.system(size: 20, weight: .bold))
+                                            Rectangle()
+                                                .frame(width:45,height:15)
+                                                .cornerRadius(7)
+                                                .foregroundColor(.red)
+                                                .overlay(
+                                                    Text("사용 중")
+                                                        .foregroundColor(.white)
+                                                        .font(.system(size: 10, weight: .thin))
+                                                )
+                                        }
+                                    }
+                                    HStack {
+                                        Image("macbook")
+                                            .resizable()
+                                            .frame(width:64,height: 64)
+                                        VStack(alignment:.leading, spacing:3) {
+                                            Text("노트북")
+                                                .font(.system(size: 20, weight: .bold))
+                                            Rectangle()
+                                                .frame(width:45,height:15)
+                                                .cornerRadius(7)
+                                                .foregroundColor(.red)
+                                                .overlay(
+                                                    Text("사용 중")
+                                                        .foregroundColor(.white)
+                                                        .font(.system(size: 10, weight: .thin))
+                                                )
+                                        }
+                                    }
+                                    HStack {
+                                        Image("macbook")
+                                            .resizable()
+                                            .frame(width:64,height: 64)
+                                        VStack(alignment:.leading, spacing:3) {
+                                            Text("노트북")
+                                                .font(.system(size: 20, weight: .bold))
+                                            Rectangle()
+                                                .frame(width:45,height:15)
+                                                .cornerRadius(7)
+                                                .foregroundColor(.red)
+                                                .overlay(
+                                                    Text("사용 중")
+                                                        .foregroundColor(.white)
+                                                        .font(.system(size: 10, weight: .thin))
+                                                )
+                                        }
+                                    }
+                                }
+                                .padding()
+                                .offset(x:-90)
                             }
-                            VStack(spacing:10) {
-                                HStack {
-                                    Image("macbook")
-                                        .resizable()
-                                        .frame(width:64,height: 64)
-                                    VStack(alignment:.leading, spacing:3) {
-                                        Text("노트북")
-                                            .font(.system(size: 20, weight: .bold))
-                                        Rectangle()
-                                            .frame(width:45,height:15)
-                                            .cornerRadius(7)
-                                            .foregroundColor(.red)
-                                            .overlay(
-                                                Text("사용 중")
-                                                    .foregroundColor(.white)
-                                                    .font(.system(size: 10, weight: .thin))
-                                            )
-                                    }
-                                }
-                                HStack {
-                                    Image("macbook")
-                                        .resizable()
-                                        .frame(width:64,height: 64)
-                                    VStack(alignment:.leading, spacing:3) {
-                                        Text("노트북")
-                                            .font(.system(size: 20, weight: .bold))
-                                        Rectangle()
-                                            .frame(width:45,height:15)
-                                            .cornerRadius(7)
-                                            .foregroundColor(.red)
-                                            .overlay(
-                                                Text("사용 중")
-                                                    .foregroundColor(.white)
-                                                    .font(.system(size: 10, weight: .thin))
-                                            )
-                                    }
-                                }
-                                HStack {
-                                    Image("macbook")
-                                        .resizable()
-                                        .frame(width:64,height: 64)
-                                    VStack(alignment:.leading, spacing:3) {
-                                        Text("노트북")
-                                            .font(.system(size: 20, weight: .bold))
-                                        Rectangle()
-                                            .frame(width:45,height:15)
-                                            .cornerRadius(7)
-                                            .foregroundColor(.red)
-                                            .overlay(
-                                                Text("사용 중")
-                                                    .foregroundColor(.white)
-                                                    .font(.system(size: 10, weight: .thin))
-                                            )
-                                    }
-                                }
-                                HStack {
-                                    Image("macbook")
-                                        .resizable()
-                                        .frame(width:64,height: 64)
-                                    VStack(alignment:.leading, spacing:3) {
-                                        Text("노트북")
-                                            .font(.system(size: 20, weight: .bold))
-                                        Rectangle()
-                                            .frame(width:45,height:15)
-                                            .cornerRadius(7)
-                                            .foregroundColor(.red)
-                                            .overlay(
-                                                Text("사용 중")
-                                                    .foregroundColor(.white)
-                                                    .font(.system(size: 10, weight: .thin))
-                                            )
-                                    }
-                                }
-                            }
-                            .padding()
-                            .offset(x:-90)
-                        }
-                    )
+                        )
+                }
+                .padding(80)
             }
-            .padding(80)
         }
     }
 }
